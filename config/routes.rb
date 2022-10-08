710ea7c973b9b19d729c481d70efd1744fef6e6c
EC2 Default User <ec2-user@ip-172-31-18-148.ap-northeast-1.compute.internal>
2022-10-08 08:02:46 +0000

    [Update]ルーティングファイルを修正

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   #利用者用
   #URL /end_users/sign_in …
   devise_for :end_users, skip: [:passwords], controllers: {
-    registrations: "public/registations",
+    registrations: "public/registrations",
     sessions: "public/sessions"
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
