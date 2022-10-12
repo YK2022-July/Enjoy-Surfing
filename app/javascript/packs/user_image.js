bab88ad0851b85f70b56fc43ffc1dbf558d30973
EC2 Default User <ec2-user@ip-172-31-18-148.ap-northeast-1.compute.internal>
2022-10-12 23:02:23 +0000

    [First]投稿の削除機能を追加

--- app/javascript/packs/user_image.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-  ('#button1').on('click', function() {
-    ("#overlay, #overlayWindow").fadeIn();
-  });
-
-  ('#button2').on('click', function() {
-    ("#overlay, #overlayWindow").fadeOut();
-  });
-};
