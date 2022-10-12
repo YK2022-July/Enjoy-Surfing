cef04721d244b9bf4067885afa03e73700d0b61a
EC2 Default User <ec2-user@ip-172-31-18-148.ap-northeast-1.compute.internal>
2022-10-12 00:04:28 +0000

    [Add]退会ユーザーのログイン制限を追加

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :reject_user, only: [:create]
 
   def after_sign_in_path_for(resource)
     my_page_path(resource)
@@ -11,11 +12,27 @@ class Public::SessionsController < Devise::SessionsController
     root_path
   end
 
+  #ゲストログイン用メソッド
   def guest_sign_in
     end_user = EndUser.guest
     sign_in end_user
-    redirect_to my_page_path(end_user), notice: 'ゲストユーザーでログインしました。'
+    flash[:notice] = "ゲストユーザーでログインしました。"
+    redirect_to my_page_path(end_user)
   end
+
+  #退会ユーザーのログイン制限メソッド
+  def reject_user
+    @end_user = EndUser.find_by(email: params[:end_user][:email])
+    if @end_user
+      if (@end_user.valid_password?(params[:end_user][:password]) && (@end_user.active_for_authentication? == false))
+        flash[:notice] = "退会済みです。再度ご登録をしてご利用ください。"
+        redirect_to new_end_user_registration_path
+      else
+        flash[:notice] = "必須項目を入力してください。"
+      end
+    end
+  end
+
   # GET /resource/sign_in
   # def new
   #   super
