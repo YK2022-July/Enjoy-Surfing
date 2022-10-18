2f78841fb3a2c3c14c7655e35bb2e48d4e1cdceb
YK2022-July <105860045+YK2022-July@users.noreply.github.com>
2022-10-18 19:16:51 +0900

    Update training_post_comments_controller.rb

--- app/controllers/public/training_post_comments_controller.rb
+++ app/controllers/public/training_post_comments_controller.rb
@@ -4,14 +4,14 @@ class Public::TrainingPostCommentsController < ApplicationController
     @comment = current_end_user.training_post_comments.new(training_post_comment_params)
     @comment.training_post_id = @training_post.id
     @comment.save
-    #render "create"
+    render "create"
   end
 
   def destroy
     @training_post = TrainingPost.find(params[:training_post_id])
     @comment = TrainingPostComment.find_by(id: params[:id], training_post_id: params[:training_post_id])
     @comment.destroy
-    #render "destroy"
+    render "destroy"
   end
 
   private
